a2679b98edbe40f2c37b9d8964022e5349754f42
shenghui wang <happydrones123@gmail.com>
2025-06-05 12:46:12 +0800

    standardization of Delay Modules

--- 0.stm32_Template/User/delay.c
+++ 0.stm32_Template/User/delay.c
@@ -1,21 +1,32 @@
 /**
- * @file delay.c
- * @brief This file implements various delay functions for precise time control, supporting microseconds, milliseconds, and seconds.
- * 
- * This file provides four delay functions: delay_us for microseconds, delay_10us for 10-microsecond delays, delay_ms for milliseconds, 
- * and delay_s for seconds. These functions use loops to generate delay, which can be used in embedded systems like STM32.
- * @note
- * us延时很不准确偏差很大
+ *******************************************************************************
+ * @file    delay.c
+ * @author  Oscar
+ * @version V1.0.0  EOL: 2025-06-10
+ * @date    2025-05-1
+ * @brief   This file provides blocking delay functions for stm32f10x devices
+ *          supporting microseconds(us),10 microseconds(10us), milliseconds(ms),
+ *          and seconds(s).
+ ******************************************************************************
+ * @note    The file contains two kinds of blocking delay implementations:
+ *          1. Loop-based delay functions
+ *          2. SysTick-based delay functions
+ * @attention 
+ *         1. The loop-based delay functions are suitable for small delays, 
+ *         2. in order to achieve high precision and accuracy,
+ *            make sure the system clock is setted at 72MHz.
+ *<h2><center>&copy; COPYRIGHT 2025 Oscar happydrones123@gmail.com </center></h2>
+ *******************************************************************************
  */
+
+/* Includes ------------------------------------------------------------------*/
 #include "delay.h"
 
 
 /**
- * @brief Delay function in microseconds
- * @param  nCount  The number of loop iterations to control the delay time
+ * @brief  Delay function in microseconds
+ * @param  nCount  The number of loop  delay time
  * @retval None
- * 
- * This function generates a delay by looping. nCount represents the number of loops, and each loop consumes a specific amount of time.
  */
 void Delay_us(u16 nCount)
 {
@@ -23,35 +34,29 @@ void Delay_us(u16 nCount)
     for(loop =0;loop<nCount; loop++)
 	{
         for (uint8_t i = 0; i < 5; i++)  ///< Inner empty loop to control the delay time
-        {
-        }
         loop++;  ///< Increment the loop counter
     };  ///< Loop until the specified delay time is reached
 }
 
 /**
- * @brief Delay function for 10 microseconds
+ * @brief  Delay function for 10 microseconds
  * @param  nCount  The number of loop iterations to control the delay time
  * @retval None
- * 
- * This function generates a 10-microsecond delay by looping. nCount represents the number of loops for the delay.
  */
 void Delay_10us(u16 nCount)
 {
-    uint16_t loop;  ///< Variable to control the number of loops
+    uint16_t loop;  
     for(loop =0;loop<nCount;loop++)
 	{
-        for (uint8_t i = 0; i < 95; i++)  ///< Inner empty loop to control the 10-microsecond delay
-        loop++;  ///< Increment the loop counter
-    }  ///< Loop until the specified delay time is reached
+        for (uint8_t i = 0; i < 95; i++)  
+        loop++;  
+    }  
 }
 
 /**
  * @brief Delay function in milliseconds
  * @param  nCount  The number of loops to control the delay time in milliseconds
  * @retval None
- * 
- * This function generates a millisecond delay by looping. nCount represents the number of milliseconds for the delay.
  */
 void Delay_ms(u16 nCount)
 {
@@ -65,67 +70,57 @@ void Delay_ms(u16 nCount)
  * @brief Delay function in seconds
  * @param  nCount  The number of loops to control the delay time in seconds
  * @retval None
- * 
- * This function generates a second-level delay by looping. nCount represents the number of seconds for the delay.
  */
 void Delay_s(u16 nCount)
 {
-    for (u32 j = 0; j < nCount; j++)  ///< Outer loop controls the number of seconds
+    for (u32 j = 0; j < nCount; j++)
     {    
-        for (u32 i = 0; i < 6605000; i++);  ///< Inner loop controls the delay for each second
+        for (u32 i = 0; i < 6605000; i++);  
     } 
 }
 
 
 
 /**
- * @brief  微秒级延时函数，基于SysTick定时器
- * 
- * 该函数使用SysTick定时器进行延时，延时的单位是微秒。它通过配置SysTick定时器的LOAD寄存器来实现延时。
- * 延时期间SysTick定时器在指定时间到达时会自动设置COUNTFLAG标志位，通过轮询该标志位来实现等待。
- * 
- * @param  X_us 延时的微秒数，范围为0到2^24-1（大约16.7秒）
+ * @brief  Delay function in microseconds, using SysTick timer
+ * @param  X_us uint is the microseconds，from 0 to 2^24-1
  * @retval None
  */
 void Systick_delay_us(uint32_t X_us)
 {
-	SysTick->LOAD = 72 * X_us - 1; /**< 设置加载值，定时器计数值为系统时钟频率 * 延时的微秒数 - 1 */
-    SysTick->VAL = 0x00;     /**< 清除当前计数值 */
-    SysTick->CTRL = 0x05;    /**< 使能SysTick定时器，选择AHB时钟源，禁用中断 */ 
-    // 等待计时完成，当COUNTFLAG标志位为1时退出
+	SysTick->LOAD = 72 * X_us - 1; 
+    SysTick->VAL = 0x00;    
+    SysTick->CTRL = 0x05;     
+ 
     while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
     
     SysTick->CTRL = 0x00000004; /**< 关闭定时器 */
 }
+/**
+ * @brief  Delay function in 10 microseconds, using SysTick timer
+ * @param  X_us uint is the microseconds，from 0 to 2^24-1
+ * @retval None
+ */
 void Systick_delay_10us(uint32_t X_us)
 {
-	SysTick->LOAD = 720 * X_us - 1; /**< 设置加载值，定时器计数值为系统时钟频率 * 延时的微秒数 - 1 */
-    SysTick->VAL = 0x00;     /**< 清除当前计数值 */
-    SysTick->CTRL = 0x05;    /**< 使能SysTick定时器，选择AHB时钟源，禁用中断 */ 
-    // 等待计时完成，当COUNTFLAG标志位为1时退出
+	SysTick->LOAD = 720 * X_us - 1; 
+    SysTick->VAL  = 0x00;     
+    SysTick->CTRL = 0x05;    
     while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
-    
-    SysTick->CTRL = 0x00000004; /**< 关闭定时器 */
+    SysTick->CTRL = 0x00000004; 
 }
 
 /**
- * @brief  毫秒级延时函数，基于SysTick定时器
- * 
- * 该函数使用SysTick定时器进行延时，延时的单位是毫秒。它通过配置SysTick定时器的LOAD寄存器来实现延时。
- * 延时期间SysTick定时器在指定时间到达时会自动设置COUNTFLAG标志位，通过轮询该标志位来实现等待。
- * 
- * @param  X_ms 延时的毫秒数，范围为0到2^24-1（大约16.7秒）
+ * @brief  Delay function in milliseconds, using SysTick timer
+ * @param  X_ms uint is the milliseconds，from 0 to 2^24-1
  * @retval None
  */
 void Systick_delay_ms(uint32_t X_ms)
 {
-    SysTick->CTRL = 0x05;    /**< 使能SysTick定时器，选择AHB时钟源，禁用中断 */
-    SysTick->LOAD = 72000 * X_ms - 1; /**< 设置加载值，定时器计数值为72,000 * 延时的毫秒数 - 1 (假设系统时钟频率为72MHz) */
-    SysTick->VAL = 0x00;     /**< 清除当前计数值 */
-    
-    // 等待计时完成，当COUNTFLAG标志位为1时退出
+    SysTick->CTRL = 0x05;           
+    SysTick->LOAD = 72000 * X_ms - 1; 
+    SysTick->VAL = 0x00;   
     while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk));
-    
-    SysTick->CTRL = 0x00000004; /**< 关闭定时器 */
+    SysTick->CTRL = 0x00000004; 
 }
 
--- 0.stm32_Template/User/delay.h
+++ 0.stm32_Template/User/delay.h
@@ -1,3 +1,25 @@
+/**
+ *******************************************************************************
+ * @file    delay.h
+ * @author  Oscar
+ * @version V1.0.0  \
+ * EOL: 2025-06-10
+ * @date    2025-05-1
+ * @brief   This file provides blocking delay functions for stm32f10x devices
+ *          supporting microseconds(us),10 microseconds(10us), milliseconds(ms),
+ *          and seconds(s).
+ ******************************************************************************
+ * @note    The file contains two kinds of blocking delay implementations:
+ *          1. Loop-based delay functions
+ *          2. SysTick-based delay functions
+ * @attention 
+ *         1. The loop-based delay functions are suitable for small delays, 
+ *         2. in order to achieve high precision and accuracy,
+ *            make sure the system clock is setted at 72MHz.
+ *<h2><center>&copy; COPYRIGHT 2025 Oscar happydrones123@gmail.com </center></h2>
+ *******************************************************************************
+ */
+
 #ifndef __DELAY_H__
 #define __DELAY_H__
 
